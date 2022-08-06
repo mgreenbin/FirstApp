251dec59d7c7afca2f5354b40dfc85c4601bea80
Mark Greenberg <magreenberg@gmail.com>
2022-08-06 13:32:11 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Mark Greenberg on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
